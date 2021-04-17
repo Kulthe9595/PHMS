aaf368666b77be46382704eb5820a8e49a90fdd4
Kulthe Shubham <48596255+Kulthe9595@users.noreply.github.com>
2021-04-17 22:09:18 +0530

    Shubham: Created index.php and registerusers.php

---
+++ connection.php
@@ -0,0 +1,50 @@
+<?php
+// Connecting to the Database
+$servername = "localhost";
+$username= "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername,$username,$password);
+global $conn;
+
+// Connection get Faild if connection was not successful
+if($conn){
+    // echo'Database Connection Ok...!!';
+}else{
+    echo'Database Connection Faild...!!!!")';
+}
+
+// Create a db
+$sql = "CREATE DATABASE  IF NOT EXISTS PHMS";   
+$result = mysqli_query($conn, $sql);
+
+// Check for the database creation success
+if($result){
+    // echo "The db was created successfully!<br>";
+}
+else{
+    echo "The db was not created successfully because of this error ---> ". mysqli_error($conn);
+}
+
+// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+// selecting our database 
+$databasename = mysqli_select_db($conn,'PHMS');  
+// Create a table in the db
+$sql1 = "CREATE TABLE IF NOT EXISTS users (
+    id int(255) PRIMARY KEY AUTO_INCREMENT,
+    username varchar(255),
+    password varchar(255)
+    )";
+$result1 = mysqli_query($conn, $sql1);
+
+// Check for the table creation success
+if($result1){
+    // echo "The Users table was created successfully!<br>";
+}
+else{
+    echo "The users table was not created successfully because of this error ---> ". mysqli_error($conn);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="Css/style.css">
+</head>
+<body>
+<!-- <center><div class="branding"><h1>PATIENT HISTORY MANAGEMENT SYSTEM</h1></center></div> -->
+    <section>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="color"></div>
+    <div class="box">
+        <div class="square" style="--i:0;"></div>
+        <div class="square" style="--i:1;"></div>
+        <div class="square" style="--i:2;"></div>
+        <div class="square" style="--i:3;"></div>
+        <div class="square" style="--i:4;"></div>
+        <div class="container">
+        <div class="form">
+            <h2>Login Form</h2>
+            <form>
+                <div class="inputBox">
+                    <input type="text" placeholder="Username">
+                </div>
+                <div class="inputBox">
+                    <input type="password" placeholder="Password">
+                </div>
+                <!-- <div class="inputBox">
+                <label >I am a:</label><br>
+			    <div class="user">
+				<label>
+					<input type="radio" name="Usertype" id="doc" value="Doctor" >
+					<span class="Buyer">Doctor</span>
+				</label>
+				<label>
+					<input type="radio" name="Usertype" id="recp" value="Receptionlist">
+					<span class="Seller">Receptionlist</span>
+				</label>
+					</div>
+				</div> -->
+                <div class="inputBox">
+                    <input type="submit" value="Login">
+                </div>
+                <p class="forget">Forget Password?<a href="#">click here</a></p>
+                <p class="forget">Don't have an account? <a href="registerusers.php">Sign up</a></p>       
+</form>
+</div>
+</div>
+</div>
+    </section>
+</body>
+</html>
