a046fe0fd7243283ba0a9d60599c68ac8a122819
mayuri wakte <mayuriwakte01@gmail.com>
2021-05-15 12:12:33 +0530

    commit change

--- Drnavbar.php
+++ Drnavbar.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHMS</title>
-    <!-- fontawesome cdn -->
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     <!-- my css -->
     <link rel="stylesheet" href="Css/RecpDashboard.css">
